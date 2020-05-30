b566518fc23577f7461f0dad7562dfa5213ac773
cegfdb <cegfdb@icloud.com>
2020-05-30 16:16:34 +0800

    m

--- lang/programming/swift/Todolist-SwiftUI/Demo/SceneDelegate.swift
+++ lang/programming/swift/Todolist-SwiftUI/Demo/SceneDelegate.swift
@@ -17,23 +17,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        
-//        if let data = UserDefaults.standard.object(forKey: "todos") as? Data {
-//                let list = NSKeyedUnarchiver.unarchiveObject(with: data) as? [Todo] ?? []
-//                for todo in list {
-//                    if !todo.checked {
-//                        main.todos.append(todo)
-//                    }
-//                }
-//            } else {
-//                main.todos = exampletodos
-//            }
-//            main.sort()
-        
-        
         let main = Main()
-        main.todos = exampletodos
+        
+        if let data = UserDefaults.standard.object(forKey: "todos") as? Data {
+                let list = NSKeyedUnarchiver.unarchiveObject(with: data) as? [Todo] ?? []
+                for todo in list {
+                    if !todo.checked {
+                        main.todos.append(todo)
+                    }
+                }
+        } else {
+            main.todos = exampletodos
+        }
+        
         main.sort()
+        
         let homeView = Home(main: main)
         
         // Use a UIHostingController as window root view controller
