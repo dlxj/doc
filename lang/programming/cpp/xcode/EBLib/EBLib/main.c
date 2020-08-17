8db667b987980348e2422cf11ceacf151207a6c1
dlxj <36914981+dlxj@users.noreply.github.com>
2020-08-17 16:46:49 +0800

    eblib hook wav ok.

--- lang/programming/cpp/xcode/EBLib/EBLib/main.c
+++ lang/programming/cpp/xcode/EBLib/EBLib/main.c
@@ -19,7 +19,9 @@
  
  另一个项目在这：
     Documents/github/doc/lang/programming/xcode/ebinfo
-    
+
+ "Ａ級"
+ 
  */
 
 #include <stdio.h>
@@ -47,6 +49,50 @@ void dataWrite(char *fname, char *dat, int siz) {
     fclose(pf);
 }
 
+EB_Error_Code iHook_WAVE(EB_Book *book, EB_Appendix *appendix,
+    void *classp, EB_Hook_Code code, int argc, const unsigned int* argv)
+{
+    if( code == EB_HOOK_END_WAVE ) {
+        // 回调只是通知你，音频已经读完了。后面没有了
+        return EB_SUCCESS;
+    }
+    
+    EB_Position spos, epos;
+    spos.page = argv[ 2 ];
+    spos.offset = argv[ 3 ];
+    epos.page = argv[ 4 ];
+    epos.offset = argv[ 5 ];
+    
+    char wavpath[1024+1];
+    char wavname[512+1];
+    sprintf(wavname, "%dx%d.wav", spos.page, spos.offset);
+    sprintf(wavpath, "%s%s", NHK, wavname);
+
+    EB_Error_Code ecode =  eb_set_binary_wave( book, &spos, &epos );
+    if (ecode != EB_SUCCESS) { ASSERT(0); }
+    
+    ssize_t len;
+    #define BUFFSIZE 1024*1024+1
+    char *buff = (char*)malloc(BUFFSIZE); // 申请1M 内存
+    for(;;) {
+        ecode = eb_read_binary(book, BUFFSIZE, buff, &len);
+        if (ecode != EB_SUCCESS) { free(buff); ASSERT(0); }
+        if (len <= 0) { ASSERT(0); }
+        if (len < BUFFSIZE) {
+            break;  // 一次读完了
+        } else {
+            ASSERT(0);  // 图片和音频按理说不应该大于1M，https://github.com/vvw/x32/blob/master/std.c ，
+            // 增加buff 的内存，继续读剩余内容？
+            // QT github qolibri 是这么做的：QByteArray b;  b += QByteArray(buff, (int)len);
+        }
+    }
+    
+    dataWrite(wavpath, buff, (int)len);
+    free(buff);
+        
+    return EB_SUCCESS;
+}
+
 // 成功回调图片
 EB_Error_Code iHookBEGIN_IN_COLOR_JPEG(EB_Book *book, EB_Appendix *appendix,
     void *classp, EB_Hook_Code code, int argc, const unsigned int* argv)
@@ -70,7 +116,7 @@ EB_Error_Code iHookBEGIN_IN_COLOR_JPEG(EB_Book *book, EB_Appendix *appendix,
     for(;;) {
         ecode = eb_read_binary(book, BUFFSIZE, buff, &len);
         if (ecode != EB_SUCCESS) { ASSERT(0); }
-        if (len <= 0) { ASSERT(0); }
+        if (len <= 0) { free(buff); ASSERT(0); }
         if (len < BUFFSIZE) {
             break;  // 一次读完了
         } else {
@@ -89,13 +135,13 @@ EB_Error_Code iHookBEGIN_IN_COLOR_JPEG(EB_Book *book, EB_Appendix *appendix,
 
 EB_Hook ihooks[] = {
   { EB_HOOK_BEGIN_IN_COLOR_JPEG, iHookBEGIN_IN_COLOR_JPEG },
+  { EB_HOOK_BEGIN_WAVE, iHook_WAVE },
+  { EB_HOOK_END_WAVE, iHook_WAVE },
   { EB_HOOK_NULL, NULL }
 };
 
 int main(int argc, const char * argv[]) {
-        
-
-
+    
     EB_Book book;
     EB_Appendix appendix;
     EB_Hookset hookset;
@@ -119,7 +165,11 @@ int main(int argc, const char * argv[]) {
     ecode = eb_set_subbook(&book, codes[0]);
 
     EB_Position pos;
-    ecode = eb_text(&book, &pos);  // first word position
+    //ecode = eb_text(&book, &pos);  // first word position
+    
+    // "Ａ級" 的position
+    pos.page = 11721;
+    pos.offset = 1490;
     ecode = eb_seek_text(&book, &pos);
 
     char buff[1024+1];
