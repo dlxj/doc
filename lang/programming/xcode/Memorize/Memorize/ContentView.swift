165454a526c89e7bce37d1eaf36842b955163234
dlxj <36914981+dlxj@users.noreply.github.com>
2020-12-30 22:12:45 +0800

    stanford swiftui 2020 code

---
+++ lang/programming/xcode/Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by v on 12/30/20.
+//  Copyright © 2020 v. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
