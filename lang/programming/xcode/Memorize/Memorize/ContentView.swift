664d005eb38e786c20ec526710758ee5a32029b7
dlxj <36914981+dlxj@users.noreply.github.com>
2021-01-07 22:15:43 +0800

    m

--- lang/programming/xcode/Memorize/Memorize/ContentView.swift
+++ lang/programming/xcode/Memorize/Memorize/ContentView.swift
@@ -10,12 +10,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack(content: {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text("👻").font(.largeTitle)
+        }).padding().foregroundColor(Color.orange)
+
     }
 }
 
+struct mainView: View {
+    var body: some View {
+        HStack(){
+            ForEach(0..<4) { index in
+                ZStack(content: {
+                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                    Text("👻").font(.largeTitle)
+                })
+            }
+            
+        }.padding().foregroundColor(Color.orange)
+            .font(.largeTitle)
+    }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+//        ContentView()
+        mainView()
     }
 }
