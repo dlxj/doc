2da1d6d17e358d714a824543a78a4e0fb149aca3
dlxj <123435@qq.com>
2021-07-07 16:01:09 +0800

    m

--- lang/programming/python/studioclassroom_FulltextSearch/insertanime/ffmpeg_cut.py
+++ lang/programming/python/studioclassroom_FulltextSearch/insertanime/ffmpeg_cut.py
@@ -28,6 +28,84 @@ import shutil
 print("cpu count: {0}".format(os.cpu_count()))
 
 
+import psycopg2
+import psycopg2.extensions as _ext
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+#from psycopg2.extensions import ISOLATION_LEVEL_DEFAULT
+
+import sqlite3 as sqlite # Python 自带的
+
+from pymysql import escape_string
+import glob
+
+import json
+import decimal
+import datetime
+
+import xmltodict
+
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return float(o)
+        elif isinstance(o, datetime.datetime):
+            return str(o)
+        super(DecimalEncoder, self).default(o)
+
+def save_json(filename, dics):
+    with open(filename, 'w', encoding='utf-8') as fp:
+        json.dump(dics, fp, indent=4, cls=DecimalEncoder, ensure_ascii=False)
+        fp.close()
+
+def load_json(filename):
+    with open(filename, encoding='utf-8') as fp:
+        js = json.load(fp)
+        fp.close()
+        return js
+
+def readstring(fname):
+    with open(fname, "r", encoding="utf-8") as fp:
+        data = fp.read()
+        fp.close()
+    return data
+
+def writestring(fname, strs):
+    with open(fname, "w", encoding="utf-8") as fp:
+        fp.write(strs)
+        fp.close()
+
+# out_bytes = subprocess.check_output([r"ffmpeg", "-i", "F:\Downloads\[Kamigami] Danganronpa Kibou no Gakuen to Zetsubou no Koukousei The Animation [1280x720 x264 AAC MKV Sub(Chs,Jap)]\[Kamigami] Danganronpa Kibou no Gakuen to Zetsubou no ...he Animation - 01 [1280x720 x264 AAC Sub(Chs,Jap)].mkv", "-map", "0:s:0", "out.srt"])
+# out_text = out_bytes.decode('utf-8')
+
+# \u4e00-\u9fa5
+
+def unchinese_remove(s):
+    return re.sub(r"[^\u4e00-\u9fa5]", "", s, flags=re.UNICODE)
+
+def chinese_remove(s):
+    return re.sub(r"[\u4e00-\u9fa5]", "", s, flags=re.UNICODE)
+
+def unjp_remove(s):
+    return re.sub(r"[^\u0800-\u4e00]", "", s, flags=re.UNICODE)
+
+def unhana_remove(s):
+    return re.sub(r"[^\u3040-\u309F^\u30A0-\u30FF]", "", s, flags=re.UNICODE)
+
+def hasHanaQ(s):
+    return len( unhana_remove(s) ) > 0
+
+def parseSrtTime(time):
+  
+  #time = "00:01:12,960 --> 00:01:14,640"
+  begin = time.split('-->')[0].strip()
+  end = time.split('-->')[1].strip()
+
+  begin = begin.replace(',', '.')
+  end = end.replace(',', '.')
+
+  return begin, end
 
 def OSXQ():
     return platform.system() == 'Darwin'
@@ -37,6 +115,180 @@ def allfname(root, ext):
     names = os.listdir(root)
     names = list(map(lambda ns:os.path.basename(ns),names))
     return names
+
+
+def readImage(fname):
+
+    fin = None
+
+    try:
+        fin = open(fname, "rb")
+        img = fin.read()
+        return img
+
+    except IOError as e:
+
+        print(f'Error {e.args[0]}, {e.args[1]}')
+        sys.exit(1)
+
+    finally:
+
+        if fin:
+            fin.close()
+
+def extractAudio(videopath, begintime, endtime):
+  # Audio: mp3 (libmp3lame), 44100 Hz, stereo, fltp, 192 kb/s (default)
+  # -vn  no video
+    # out_bytes = subprocess.check_output([r"ffmpeg", "-y", "-i", videopath, "-vn", "-ss", begintime, "-to", endtime, "-acodec", "mp3", \
+    #   "-ar", "44100", "-ac", "2", "-b:a", "192k", \
+    #     "tmp.mp3"])
+    
+    out_bytes = subprocess.check_output([r"ffmpeg", "-y", "-hide_banner", "-loglevel", "error", "-i", videopath, "-vn", "-ss", begintime, "-to", endtime, "-acodec", "mp3", \
+      "-ar", "44100", "-ac", "2", "-b:a", "192k", \
+        "tmp.mp3"])
+
+    out_text = out_bytes.decode('utf-8')
+    bts = readImage("tmp.mp3")
+    os.remove("tmp.mp3")
+    return bts
+
+# success
+# hevc 表示使用h.265 编码
+# ffmpeg -y -ss 00:01:12.960 -to 00:01:14.640  -i t.mkv  -codec:v hevc -acodec mp3 -ar 44100 -ac 2 -b:a 192k t.ts
+def extractVideo(videopath, begintime, endtime):
+  # Audio: mp3 (libmp3lame), 44100 Hz, stereo, fltp, 192 kb/s (default)
+  # -vn  no video
+    # out_bytes = subprocess.check_output([r"ffmpeg", "-y", "-i", videopath, "-vn", "-ss", begintime, "-to", endtime, "-acodec", "mp3", \
+    #   "-ar", "44100", "-ac", "2", "-b:a", "192k", \
+    #     "tmp.mp3"])
+    
+    # out_bytes = subprocess.check_output([r"ffmpeg", "-y", "-hide_banner", "-loglevel", "error", "-i", videopath, "-codec:v", "hevc",  "-ss", begintime, "-to", endtime, "-acodec", "mp3", \
+    #   "-ar", "44100", "-ac", "2", "-b:a", "192k", \
+    #     "tmp.ts"])
+
+    """
+    # jsmpeg.js normal play
+    ! ffmpeg -i t.mkv -y -ss 00:01:12.960 -to 00:01:14.640 -f mpegts -codec:v mpeg1video -b:v 1500k -s 960x540 -r 30 -bf 0 -codec:a mp2 -ar 44100 -ac 2 -b:a 192k -vf subtitles=t.mkv t.ts
+    """
+    # out_bytes = subprocess.check_output([r"ffmpeg", "-y", "-hide_banner", "-loglevel", "error", "-i", videopath, "-f", "mpegts", "-codec:v", "mpeg1video",  "-ss", begintime, "-to", endtime, "-acodec", "mp3", \
+    #   "-ar", "44100", "-ac", "2", "-b:a", "192k", \
+    #     "tmp.ts"])
+
+    tmpmkv = "tmp.mkv"
+    if ( os.path.exists(tmpmkv) ):
+      os.unlink("tmp.mkv")
+    os.symlink(videopath, "tmp.mkv")
+    
+    #cmd = f'ffmpeg -i \"{videopath}\" -y -ss {begintime} -to {endtime} -f mpegts -codec:v mpeg1video -b:v 1500k -s 960x540 -r 30 -bf 0 -codec:a mp2 -ar 44100 -ac 2 -b:a 192k -vf subtitles=\"{videopath}\" tmp.ts' #% (videopath,videopath)#('t.mkv', 't.mkv')
+    cmd = f'ffmpeg -i \"{videopath}\" -y -hide_banner -loglevel error -ss {begintime} -to {endtime} -f mpegts -codec:v mpeg1video -b:v 1500k -s 960x540 -r 30 -bf 0 -codec:a mp2 -ar 44100 -ac 2 -b:a 192k -vf subtitles=\"tmp.mkv\" tmp.ts' #% (videopath,videopath)#('t.mkv', 't.mkv')
+
+    out_bytes = subprocess.check_output(cmd, shell=True)
+
+    
+
+    out_text = out_bytes.decode('utf-8')
+    bts = readImage("tmp.ts")
+    #os.remove("tmp.t")
+    return bts
+
+
+def cutAnime(animename, seasion, frtname, videoname, videopath):
+    dic_chs = {}
+
+    currDir = os.path.dirname(os.path.abspath(__file__))
+
+    
+    fsrt = os.path.join(currDir, frtname)
+    strs = "\n"+readstring(fsrt)+"\n"
+    iters = re.finditer(r"\n\d+\n", strs, re.DOTALL)
+    poss = [ i.span() for i in iters ]
+
+
+    #animename = 'Danganronpa'
+
+    chinese = []
+    jpanese = []
+    for i in range( len(poss) ):
+        (start, end) = poss[i]
+        
+        n = strs[start:end]
+
+        content = None
+        if i == ( len(poss) - 1 ):
+            content = strs[ end : len(strs) ]
+        else:
+            content = strs[ end : poss[i+1][0] ]
+        
+        arr = content.strip().split('\n')
+        if len(arr) < 2:
+          continue
+        time = content.strip().split('\n')[0]
+        content = content.strip().split('\n')[1]
+
+
+
+        
+
+        if len( re.compile(r'size=.+>(.+?)\<\/font\>').findall(content) ) > 0:
+          subtitle = re.compile(r'size=.+>(.+?)\<\/font\>').findall(content)[0]
+        else:
+          content = re.compile(r"""face=".+?\"""").sub('', content)
+          content = re.compile(r"""size="\d+\"""").sub('', content)
+          content = re.compile(r"""color=".+?\"""").sub('', content)
+          content = re.compile(r"""<font.+?>""").sub('', content)
+          content = re.compile(r"""{\\an7}""").sub('', content)
+          
+          subtitle = content
+
+        # elif match := re.compile("""(<font face=".+?" size="\d+"><font size="\d+">).+?""").search(content):
+        #   m = match.group(1)
+        #   subtitle = content.replace(m, "")
+        # elif match := re.compile(r"""(<font face=".+?" size="\d+"><font face=".+?">{\\an\d+}<font size="\d+">).+?""").search(content):
+        #   m = match.group(1)
+        #   subtitle = content.replace(m, "")
+        # else:
+        #   raise RuntimeError('some err')
+
+        subtitle = subtitle.replace('<b>','').replace('</b>','')
+        
+        chrst = chardet.detect(subtitle.encode())
+
+        
+        if chrst['encoding'] == 'ascii' and chrst['confidence'] > 0.99:
+            b = 1
+        elif hasHanaQ(subtitle): # 有jia ming 的是jp
+          jpanese.append( (subtitle, time) )
+        else:
+            tmp = unchinese_remove(subtitle)
+            if tmp != "":
+              
+              if time in dic_chs:
+                continue
+                #raise RuntimeError('some err')
+              
+              dic_chs[time] = subtitle
+
+              tmp = "|".join( list(tmp) )
+              begin = time.split('-->')[0].strip()
+              end = time.split('-->')[1].strip()
+              allhasjpduyingQ = False
+
+
+    jpanese = sorted(jpanese, key=lambda tu: tu[1], reverse=False)  # sort by time asc
+
+
+    count = 0
+    for idx, tu in enumerate(jpanese):
+        j = tu[0]
+        zh = ""
+        #tags = tagger.parse(j)
+        t = tu[1]
+        begintime, endtime = parseSrtTime(t)
+        bmp3_audio = extractAudio(videopath, begintime, endtime)
+        #bts = psycopg2.Binary(bts)
+        #bts_video = extractVideo(videopath, begintime, endtime)
+        #bts_video = psycopg2.Binary(bts_video)
+
 
 
 def cut(rootdir, fname, idx):
@@ -63,20 +315,15 @@ def cut(rootdir, fname, idx):
     out_bytes = subprocess.check_output([r"ffmpeg", "-y", "-loglevel", "error", "-i", fname, "-map", "0:s:0", frtname_temp])
     out_text = out_bytes.decode('utf-8')
 
-    # if ( os.path.exists( newdir ) ):
-    #     if ( os.path.exists( frtname_temp ) ):
-    #         with open(frtname, 'w') as f:
-    #             f.write('original')
-
     shutil.copy2(frtname_temp, newdir)
     os.remove(frtname_temp)
-        
-
 
     print("### idx {0} doen.", idx)
 
     animename = 'Danganronpa'
     seasion = '01'
+
+    cutAnime(animename, seasion, frtname, videoname, videopath)
 
 
 if __name__ == "__main__":
@@ -128,15 +375,4 @@ if __name__ == "__main__":
 
     fnames = sorted(fnames, key=lambda s: s, reverse=False)  # sort by name
 
-    ret = Parallel(n_jobs=1, verbose=10)(delayed(cut)(root, fnames[i], i) for i in range(len(fnames)-1)) # os.cpu_count()
-
-    # for fname in fnames:
-    #   videoname = fname
-    #   frtname = f"{fname}.srt"
-    #   fname = os.path.join( root, fname )
-    #   videopath = os.path.join( root, videoname )
-    #   out_bytes = subprocess.check_output([r"ffmpeg", "-y", "-loglevel", "error", "-i", fname, "-map", "0:s:0", frtname])
-    #   out_text = out_bytes.decode('utf-8')
-
-    #   animename = 'Danganronpa'
-    #   seasion = '01'
+    ret = Parallel(n_jobs=1, verbose=10)(delayed(cut)(root, fnames[i], i) for i in range(1)) # os.cpu_count() # len(fnames)-1)
