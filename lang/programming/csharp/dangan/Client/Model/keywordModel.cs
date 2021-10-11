3d494c90601920c5e35a71fa95f7448122b525e5
dlxj <123435@qq.com>
2021-10-11 09:46:47 +0800

    m

--- lang/programming/csharp/dangan/Client/Model/keywordModel.cs
+++ lang/programming/csharp/dangan/Client/Model/keywordModel.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using System.ComponentModel.DataAnnotations;
-
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace dangan.Client.Model
 {
@@ -13,5 +14,21 @@ namespace dangan.Client.Model
         [Required]
         [StringLength(30, ErrorMessage = "keyword is too long.")]
         public string keyword { get; set; }
+    }
+
+    public class rowModel
+    {
+        [JsonPropertyName("id")]
+        public string id { get; set; }
+
+        [JsonPropertyName("jp")]
+        public string jp { get; set; }
+
+        [JsonPropertyName("zh")]
+        public string zh { get; set; }
+
+
+        [JsonPropertyName("time")]
+        public string time { get; set; }
     }
 }
