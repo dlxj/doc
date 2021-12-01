7c5969093c9a9e4689806c6258b630824b4232ae
guandong <1623088350@qq.com>
2021-12-01 16:49:25 +0800

    Update kuro.js

--- lang/programming/nodejs/psql/kuro.js
+++ lang/programming/nodejs/psql/kuro.js
@@ -2,11 +2,12 @@
 let kuromoji = require('kuromoji')
 
 kuromoji.builder({ dicPath: "./dicts/neologd/" }).build((err, tokenizer) => {
+   
    let a = tokenizer.tokenize('abc')
 
-    console.log(a)
+   console.log(a)
 
-    b = 1
+   b = 1
 
 })
 
