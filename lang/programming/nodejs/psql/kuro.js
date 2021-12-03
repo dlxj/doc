9c5ea2a26c9e1d56881b22c05e124b4b21d071f2
guandong <1623088350@qq.com>
2021-12-03 09:57:07 +0800

    m

--- lang/programming/nodejs/psql/kuro.js
+++ lang/programming/nodejs/psql/kuro.js
@@ -2,7 +2,7 @@
 // let kuromoji = require('kuromoji')
 
 // kuromoji.builder({ dicPath: "./dicts/neologd/" }).build((err, tokenizer) => {
-   
+
 //    let a = tokenizer.tokenize('abc')
 
 //    console.log(a)
@@ -14,97 +14,104 @@
 
 (async () => {
 
-   let arr = require('fs').readFileSync('./data.json', {encoding:'utf8', flag:'r'})
+  let arr = require('fs').readFileSync('./data.json', { encoding: 'utf8', flag: 'r' })
 
-   arr = JSON.parse(arr)
+  arr = JSON.parse(arr)
 
-   // let kuromoji = require('kuromoji')
+  // let kuromoji = require('kuromoji')
 
-   // let [ tokenizer, ms ] = await new Promise(function (resolve) {
+  // let [ tokenizer, ms ] = await new Promise(function (resolve) {
 
-   //   kuromoji.builder({ dicPath: "./dicts/neologd/" }).build((err, tokenizer) => {
+  //   kuromoji.builder({ dicPath: "./dicts/neologd/" }).build((err, tokenizer) => {
 
-   //     if (err) resolve([ null, err.toString() ] )
+  //     if (err) resolve([ null, err.toString() ] )
+
+  //     resolve([ tokenizer, ''])
+
+  //  })
+
+  // })
+
+
+
+
+
+  // let a = tokenizer.tokenize(arr[0])
+
+  // let basics = ''
+  // let surfaces = ''
+  // let readings = ''
+
+  // for (let i = 0; i < a.length; i++) {
+
+  //    let item = a[i]
+
+  //    let basic_form = item.basic_form
+  //    let surface_form = item.surface_form
+  //    let reading =  item.reading
+
+  //    basics += basic_form + " "
+  //    surfaces += surface_form + " "
+  //    readings += reading + " "
+
+  // }
+
+
+
+  // console.log(a)
+
+
+
+  // //var MeCab = new require('mecab-async-winfix')
+  // var MeCab = new require('mecab-async')
+  // , mecab = new MeCab()
+
+  // //mecab.ENCODING = 'UTF-8'
+
+
+  // let [ result, ms2 ] = await new Promise(function (resolve) {
+
+  //    mecab.parse('ここ', function(err, result) {
+
+  //       if (err) resolve([ null, err.toString() ] )
+
+  //       resolve([ result, ''])
+
+  //    })
+
+
+  //  })
+
+
+  // console.log(result)
+
+
+  var MeCab = new require('mecab-async')
+  var mecab = new MeCab()
+  //MeCab.command = "mecab -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd"
+  //var text = arr[0]
   
-   //     resolve([ tokenizer, ''])
-    
-   //  })
-
-   // })
+  
 
 
 
-   
-
-   // let a = tokenizer.tokenize(arr[0])
-
-   // let basics = ''
-   // let surfaces = ''
-   // let readings = ''
-
-   // for (let i = 0; i < a.length; i++) {
-
-   //    let item = a[i]
-
-   //    let basic_form = item.basic_form
-   //    let surface_form = item.surface_form
-   //    let reading =  item.reading
-
-   //    basics += basic_form + " "
-   //    surfaces += surface_form + " "
-   //    readings += reading + " "
-
-   // }
-
-   
-
-   // console.log(a)
 
 
-
-   // //var MeCab = new require('mecab-async-winfix')
-   // var MeCab = new require('mecab-async')
-   // , mecab = new MeCab()
-
-   // //mecab.ENCODING = 'UTF-8'
- 
-
-   // let [ result, ms2 ] = await new Promise(function (resolve) {
-
-   //    mecab.parse('ここ', function(err, result) {
-
-   //       if (err) resolve([ null, err.toString() ] )
-
-   //       resolve([ result, ''])
-
-   //    })
-
- 
-   //  })
-
-   
-   // console.log(result)
+  let str = arr[0]
+  
+  MeCab.parseFormat(str, function (err, morphs) {
+    if (err) throw err;
+    morphs.map(function (morph) {
 
 
-   var MeCab = new require('mecab-async')
-   var mecab = new MeCab();
-       //MeCab.command = "mecab -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd"
-       var text = 'こんにちは、サミュエルLジャクソンです。'
-       //注：パースコマンドを利用する時 "MeCab.~"と大文字にしないと動かないみたいです
-       MeCab.parseFormat(text, function(err, morphs) {
-           if (err) throw err;
-           morphs.map(function(morph) {
-           if (morph.lexical === '感動詞') {
-             console.log(morph.lexical + ' : ' + morph.original);
-           }
-           if (morph.lexical === '名詞') {
-             console.log(morph.lexical + ' : ' +morph.original);
-           }
-       });
-       });
+      console.log(morph.lexical + ' : ' + morph.original)
 
 
-   b  = 1
+      a = 1
+
+    })
+  })
+
 
 })()
 
