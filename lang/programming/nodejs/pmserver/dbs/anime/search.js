6eb9e24cc380f7a44570b706ede4deebbffe51bc
guandong <guandong@qq.com>
2022-02-22 09:23:40 +0800

    m

--- lang/programming/nodejs/pmserver/dbs/anime/search.js
+++ lang/programming/nodejs/pmserver/dbs/anime/search.js
@@ -2,6 +2,10 @@ module.exports = {
     name: `search`,
     author: ``,
     params: {
+        tablename: {
+            type: 'string',
+            remark: ''
+        },
         keywd: {
             type: 'string',
             remark: ''
@@ -9,7 +13,7 @@ module.exports = {
     },
     remark: ``,
     sql: `
-        SELECT id, jp_ruby as jp, zh, p.begintime as time, name, seasion FROM pokemon p  WHERE p.v_jp @@ to_tsquery($(keywd))  ORDER BY RANDOM()  LIMIT 3;
+        SELECT id, jp_ruby as jp, zh, p.begintime as time, name, seasion FROM $$(tablename) p  WHERE p.v_jp @@ to_tsquery($(keywd))  ORDER BY RANDOM()  LIMIT 3;
     `,
   }
 
