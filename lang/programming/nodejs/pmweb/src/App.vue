c3c60f7cd79c18d4e8adff366274d14921f26cef
guandong <guandong@qq.com>
2022-03-02 18:19:24 +0800

    m

--- lang/programming/nodejs/pmweb/src/App.vue
+++ lang/programming/nodejs/pmweb/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
+    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -10,8 +10,6 @@ export default {
   name: 'App'
 }
 </script>
-
-<!--
 
 <style>
 #app {
@@ -23,5 +21,3 @@ export default {
   margin-top: 60px;
 }
 </style>
-
--->
