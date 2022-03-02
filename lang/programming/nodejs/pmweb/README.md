b29b55669208a903d4f19f5ede1e90b0210e2978
dlxj <123468935@qq.com>
2022-03-02 22:17:32 +0800

    m

--- lang/programming/nodejs/pmweb/README.md
+++ lang/programming/nodejs/pmweb/README.md
@@ -17,6 +17,9 @@ npm run build
 # build for production and view the bundle analyzer report
 npm run build --report
 
+# run unit tests
+npm run unit
+
 # run e2e tests
 npm run e2e
 
--- lang/programming/nodejs/pmweb/src/components/HelloWorld.vue
+++ lang/programming/nodejs/pmweb/src/components/HelloWorld.vue
@@ -1,100 +1,125 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    keywd
+    <!-- <input type="text" v-for="(item,i) of items" v-model="items[i]" :key="i"> <button @click="search">search</button> -->
+    <input v-model="keywdModel.keywd" placeholder="edit me" />  <button @click="search">search</button>
+    <p>keywd is: {{ keywdModel.keywd }}</p>
+
+    <!-- v-if 是条件渲染，每次状态更新都会重新删除或者创建元素，但v-if有较高的切换性能消耗 -->
+    <!-- Vue 官方中不推荐v-for 和v-if 在同一标签中共同使用。因此，给上述示例代码外面加上一层div，isListShow 为true 时创建，为false 时销毁 -->
+    <!-- <div class="list_main" v-if="isListShow">
+      <ul id="example-1">
+        <li v-for="item in items" :key="item.message">
+          {{ item.message }}
+        </li>
+      </ul>
+    </div> -->
+
+  </div>
+</template>
+
+<script>
+
+// import config from '@/config.js'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      keywdModel: { keywd: '' },
+      items: []
+    }
+  },
+  computed: {
+    itemNum: function () {
+      return this.items.length
+    }
+  },
+  methods: {
+
+    search () {
+      // let debugQ = config.debug;
+      this.$set(this.keywdModel, 'keywd', 'aaaa')
+      console.log('hited.')
+    }
+
+  }
+  // watch: {
+
+  //   items (newVal) {
+  //     isListShow = false
+  //     this.$nextTick(() => {
+  //       isListShow = true
+  //     })
+  //   }
+
+  // }
+}
+</script>
+
+<!--
+
+<template>
+  <div>
+    <input type="text" v-for="(item,i) of items" v-model="items[i]" :key="i">
+    <button @click="onAdd">添加</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      items: []
+    }
+  },
+  methods: {
+    onAdd() {
+      this.items.push('')
     }
   }
 }
 </script>
 
+// template
+<input type="text" v-model="computeValue">
+
+// js
+data () {
+    return {
+        form: {
+            computeValue: ''
+        }
+    }
+}
+computed: {
+    computeValue: {
+      get () {
+        return this.form.computeValue
+      },
+      set (val) {
+        this.form.computeValue = val.replace(/[9]/g, 'a')
+      }
+    }
+}
+
+      // 1. 异步渲染，$nextTick 待 DOM 渲染完再回调
+      // 2. 页面渲染时会将 data 的修改做整合，多次 data 修改只会渲染一次
+      this.$nextTick(() => {
+        // 获取 DOM 元素
+        // const ulElem = this.$refs.ul1
+        // 如果没加$nextTick,点击第一次，li的节点是3个（为何不是6个），因为dom是异步渲染，data数据改变不会立刻渲染
+        // console.log( ulElem.childNodes.length )
+        items = [
+          { message: 'Xoo' },
+          { message: 'Yar' }
+        ]
+        console.log('hited.')
+      })
+-->
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<!--
 <style scoped>
 h1, h2 {
   font-weight: normal;
@@ -111,3 +136,4 @@ a {
   color: #42b983;
 }
 </style>
+-->
