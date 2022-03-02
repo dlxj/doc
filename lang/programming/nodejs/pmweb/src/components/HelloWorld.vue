64220ebc3f08d96c21c608aec48fcc3479c18875
guandong <guandong@qq.com>
2022-03-02 10:20:36 +0800

    Update HelloWorld.vue

--- lang/programming/nodejs/pmweb/src/components/HelloWorld.vue
+++ lang/programming/nodejs/pmweb/src/components/HelloWorld.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="hello">
-    hello, world!
-    <input v-model="message" placeholder="edit me">
+    keywd
+    <input v-model="message" placeholder="edit me" />  <button @click="search">search</button>
     <p>Message is: {{ message }}</p>
+
+    <ul id="example-1">
+      <li v-for="item in items" :key="item.message">
+        {{ item.message }}
+      </li>
+    </ul>
+
   </div>
 </template>
 
@@ -11,8 +18,27 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      message: ''
+      message: '',
+      items: [
+        { message: 'Foo' },
+        { message: 'Bar' }
+      ]
     }
+  },
+  methods: {
+
+    search () {
+      // 1. 异步渲染，$nextTick 待 DOM 渲染完再回调
+      // 2. 页面渲染时会将 data 的修改做整合，多次 data 修改只会渲染一次
+      this.$nextTick(() => {
+        // 获取 DOM 元素
+        // const ulElem = this.$refs.ul1
+        // 如果没加$nextTick,点击第一次，li的节点是3个（为何不是6个），因为dom是异步渲染，data数据改变不会立刻渲染
+        // console.log( ulElem.childNodes.length )
+        console.log('hited.')
+      })
+    }
+
   }
 }
 </script>
