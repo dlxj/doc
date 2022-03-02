a28cf9613817a9e3c130938f88a26ea3e2b2b3e6
guandong <guandong@qq.com>
2022-03-02 10:44:09 +0800

    Update HelloWorld.vue

--- lang/programming/nodejs/pmweb/src/components/HelloWorld.vue
+++ lang/programming/nodejs/pmweb/src/components/HelloWorld.vue
@@ -4,25 +4,35 @@
     <input v-model="message" placeholder="edit me" />  <button @click="search">search</button>
     <p>Message is: {{ message }}</p>
 
-    <ul id="example-1">
-      <li v-for="item in items" :key="item.message">
-        {{ item.message }}
-      </li>
-    </ul>
+    <!-- v-if 是条件渲染，每次状态更新都会重新删除或者创建元素，但v-if有较高的切换性能消耗 -->
+    <!-- Vue 官方中不推荐v-for 和v-if 在同一标签中共同使用。因此，给上述示例代码外面加上一层div，isListShow 为true 时创建，为false 时销毁 -->
+    <div class="list_main" v-if="isListShow">
+      <ul id="example-1">
+        <li v-for="item in items" :key="item.message">
+          {{ item.message }}
+        </li>
+      </ul>
+    </div>
 
   </div>
 </template>
 
 <script>
+
+let isListShow = true
+
+let items = [
+  { message: 'Foo' },
+  { message: 'Bar' }
+]
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
       message: '',
-      items: [
-        { message: 'Foo' },
-        { message: 'Bar' }
-      ]
+      isListShow,
+      items
     }
   },
   methods: {
@@ -35,6 +45,10 @@ export default {
         // const ulElem = this.$refs.ul1
         // 如果没加$nextTick,点击第一次，li的节点是3个（为何不是6个），因为dom是异步渲染，data数据改变不会立刻渲染
         // console.log( ulElem.childNodes.length )
+        items = [
+          { message: 'Xoo' },
+          { message: 'Yar' }
+        ]
         console.log('hited.')
       })
     }
