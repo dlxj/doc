fe9b868d1a48acbca01942cfd458d7ce92a049cd
guandong <guandong@qq.com>
2022-03-08 18:01:21 +0800

    Update h.py

--- lang/programming/python/opencv/面对直线你说霍夫线变换是万能的吗/h.py
+++ lang/programming/python/opencv/面对直线你说霍夫线变换是万能的吗/h.py
@@ -16,49 +16,93 @@ if __name__ == '__main__':
 
     imgData = np.fromfile('./填空题.png', dtype=np.uint8)
     img = cv2.imdecode(imgData, -1)
+    #img = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
+    print(type(img))
 
     w = img.shape[0]
     h = img.shape[1]
 
     # slice 子矩阵，既剪裁图像
-    img_crop = img[ 0:w-30, 0:h-70]
+    img_crop = img[0:w-30, 0:h-70]
+
+    # 二值化
+    ret,img_binary = cv2.threshold(img_crop, 92, 255, cv2.THRESH_BINARY_INV)
+    #imshow("1:二值操作", binaryImage)
+
+    # 开操作(将文字这些密集的“孔洞”给腐蚀掉，仅留下直线)
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 2))  # 定义了20*2 大小的矩形核
+    img_opening = cv2.morphologyEx(img_binary, cv2.MORPH_OPEN, rect_kernel)
+
+    # 膨胀加粗
+    rect_kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))  # 定义了20*2 大小的矩形核
+    img_dilate = cv2.dilate(img_opening, rect_kernel2)
+
+
+
 
     cv2.imshow("origin", img)
     cv2.imshow("croped", img_crop)
+    cv2.imshow("binary", img_binary)
+    cv2.imshow("opening", img_opening)
+    cv2.imshow("dilate", img_dilate)
+
+
+
     cv2.waitKey(0)
 
 
-    # img = cv2.mat_wrapper.Mat(img)
 
-    #crop_img = img[0:y+h, 0:x+w]
+"""
 
-    #img2 = img.crop((0, 0, 10, 10))  # (left, upper, right, lower)
-
-    #img(cv2.Rect(0, 0, 10, 10))
-
-    # 剪裁图片
-    #Mat roiImage = srcImage(Rect(0, 0, srcImage.cols - 70, srcImage.rows - 30));
-    #imshow("0:抠图操作", roiImage)
-
-
-
-
+cpp origin
 
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-# using namespace std;
-# using namespace cv;
+using namespace std;
+using namespace cv;
 
-# int main()
-# {
-#   Mat srcImage, dstImage, binaryImage;
-#   srcImage = imread("原图.png",0);  
-#   imshow("原图", srcImage);
+int main()
+{
+  Mat srcImage, dstImage, binaryImage;
+  srcImage = imread("原图.png",0);  
+  imshow("原图", srcImage);
   
-#   waitKey(0);
-#   return 0;
-# }
+  waitKey(0);
+  return 0;
+}
 
+ //剪裁图片
+  Mat roiImage = srcImage(Rect(0, 0, srcImage.cols - 70, srcImage.rows - 30));
+  imshow("0:抠图操作", roiImage);
+
+    //对图像进行二值化
+  threshold(roiImage, binaryImage, 92, 255, THRESH_BINARY_INV );
+  imshow("1:二值操作", binaryImage);
+
+
+  Mat morhpImage;
+  Mat kernel = getStructuringElement(MORPH_RECT, Size(20, 2), Point(-1, -1));//自定义一个核
+  morphologyEx(binaryImage, morhpImage, MORPH_OPEN, kernel, Point(-1, -1));//开操作
+  imshow("2:开操作", morhpImage);
+
+
+  Mat dilateImage;
+  kernel = getStructuringElement(MORPH_RECT, Size(3, 3), Point(-1, -1));
+  dilate(morhpImage, dilateImage, kernel);
+  imshow("3:膨胀操作", dilateImage);
+
+  
+  vector<Vec4i> lines;
+  HoughLinesP(dilateImage, lines, 1, CV_PI / 180.0, 30, 20.0, 0);
+  dstImage = srcImage.clone();
+  cvtColor(dstImage, dstImage, COLOR_GRAY2BGR);
+  for (size_t t = 0; t < lines.size(); t++) {
+    Vec4i ln = lines[t];
+    line(dstImage, Point(ln[0], ln[1]), Point(ln[2], ln[3]), Scalar(0, 0, 255), 2, 8, 0);
+  }
+  imshow("4:绘制直线", dstImage);
+
+"""
 
