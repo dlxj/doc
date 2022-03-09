ab30e8c7f0717e1a280d7d227cc7eb2addedc202
guandong <guandong@qq.com>
2022-03-09 15:07:00 +0800

    m

---
+++ lang/programming/pytorch/词嵌入/pytorch-continuous-bag-of-words/cbow.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+
+def make_context_vector(context, word_to_ix):
+    idxs = [word_to_ix[w] for w in context]
+    return torch.tensor(idxs, dtype=torch.long)
+
+CONTEXT_SIZE = 2  # 2 words to the left, 2 to the right
+EMDEDDING_DIM = 100
+
+raw_text = """We are about to study the idea of a computational process.
+Computational processes are abstract beings that inhabit computers.
+As they evolve, processes manipulate other abstract things called data.
+The evolution of a process is directed by a pattern of rules
+called a program. People create programs to direct processes. In effect,
+we conjure the spirits of the computer with our spells.""".split()
+
+
+# By deriving a set from `raw_text`, we deduplicate the array
+vocab = set(raw_text)       # 词集合
+vocab_size = len(vocab)     # 元素个数
+
+# 总共49 个不同的词，每一个词每配一个数(0 ~ 48)
+word_to_ix = {word:ix for ix, word in enumerate(vocab)}  # 词 到 数
+ix_to_word = {ix:word for ix, word in enumerate(vocab)}  # 数 到 词
+
+# 构建训练数据，由上下文(context, 前后各两个词) 和目标词(target, 机器需要根据上下文猜的词) 组成
+data = []
+for i in range(2, len(raw_text) - 2):
+    context = [raw_text[i - 2], raw_text[i - 1],
+               raw_text[i + 1], raw_text[i + 2]]
+    target = raw_text[i]
+    data.append((context, target))
+
+
+class CBOW(torch.nn.Module):
+    def __init__(self, vocab_size, embedding_dim):
+        super(CBOW, self).__init__()
+
+        #out: 1 x emdedding_dim
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.linear1 = nn.Linear(embedding_dim, 128)
+        self.activation_function1 = nn.ReLU()
+        
+        #out: 1 x vocab_size
+        self.linear2 = nn.Linear(128, vocab_size)
+        self.activation_function2 = nn.LogSoftmax(dim = -1)
+        
+
+    def forward(self, inputs):
+        embeds = sum(self.embeddings(inputs)).view(1,-1)
+        out = self.linear1(embeds)
+        out = self.activation_function1(out)
+        out = self.linear2(out)
+        out = self.activation_function2(out)
+        return out
+
+    def get_word_emdedding(self, word):
+        word = torch.tensor([word_to_ix[word]])
+        return self.embeddings(word).view(1,-1)
+
+
+model = CBOW(vocab_size, EMDEDDING_DIM)
+
+loss_function = nn.NLLLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+
+#TRAINING
+for epoch in range(50):
+    total_loss = 0
+
+    for context, target in data:
+        context_vector = make_context_vector(context, word_to_ix)  
+
+        log_probs = model(context_vector)
+
+        total_loss += loss_function(log_probs, torch.tensor([word_to_ix[target]]))
+
+    #optimize at the end of each epoch
+    optimizer.zero_grad()
+    total_loss.backward()
+    optimizer.step()
+
+#TESTING
+context = ['People','create','to', 'direct']
+context_vector = make_context_vector(context, word_to_ix)
+a = model(context_vector)
+
+#Print result
+print(f'Raw text: {" ".join(raw_text)}\n')
+print(f'Context: {context}\n')
+print(f'Prediction: {ix_to_word[torch.argmax(a[0]).item()]}')
--- lang/programming/深入理解神经网络：从逻辑回归到CNN.md
+++ lang/programming/深入理解神经网络：从逻辑回归到CNN.md
@@ -5343,6 +5343,12 @@ one hot 独热，是最原始的用来表示字、词的方式（每一个词分
 
 
 
+### pytorch 实现cbow
+
+- https://github.com/FraLotito/pytorch-continuous-bag-of-words
+
+
+
 
 
 ### 一切要从one hot说起
