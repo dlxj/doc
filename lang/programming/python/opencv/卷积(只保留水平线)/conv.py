1d5a2be2095e5328f55e72aa780580c9d2476a43
guandong <guandong@qq.com>
2022-03-30 18:26:55 +0800

    m

--- lang/programming/python/opencv/卷积(只保留水平线)/conv.py
+++ lang/programming/python/opencv/卷积(只保留水平线)/conv.py
@@ -9,9 +9,9 @@ image = cv2.imdecode(np.fromfile('./cleaned.jpg',dtype=np.uint8), -1)
 
 # 卷积, 只保留水平线
 kernel1 = np.array([ [1], [-1] ])
-img = cv2.filter2D(src=image, ddepth=-1, kernel=kernel1)
+img_conv = cv2.filter2D(src=image, ddepth=-1, kernel=kernel1)
   
 cv2.imshow('Original', image)
-cv2.imshow('conv', img)
+cv2.imshow('conv', img_conv)
   
 cv2.waitKey()
