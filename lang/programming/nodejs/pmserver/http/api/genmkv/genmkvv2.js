0db3f25a88bd5ccf8d2b589b3a4e165f5bbeb192
dlxj <123468935@qq.com>
2022-04-20 00:21:36 +0800

    Create genmkvv2.js

---
+++ lang/programming/nodejs/pmserver/http/api/genmkv/genmkvv2.js
@@ -0,0 +1,70 @@
+
+let path = require('path')
+let fs = require('fs')
+
+module.exports = {
+    name: 'genmkv',
+    remark: '',
+    params: {
+        type: {
+            type: 'enum',
+            range:["anime","drama"],
+            remark: '',
+            default:'anime'
+        }
+    },
+    async handler({type}) {
+
+        let platform = process.platform
+        let root_vd = global.config.root_vd[platform]
+
+        let kvs = this.libs.files.allfiles(root_vd, 'mkv', ['pokemon_amazon', 'S01'])  // E:\videos\anime\pokemon\amazon\S01
+
+        let m4s = this.libs.files.allfiles(root_vd, 'mp4', ['pokemon_tw', 'S01'])  // anime\pokemon_tw\S01
+
+        let srts = this.libs.files.allfiles(global.root_subtitles, 'srt', ['amazon', 'pokemon', 'srt', 'S01'])  // amazon\pokemon
+
+        for (let kvpath of kvs) {
+
+            let { base:kvbase, dir:kvdir, ext:kvext, name:kvname, root:kvroot } = path.parse(kvpath)
+
+            let kvseason = this.libs.files.season(kvpath)
+            if (kvseason == null) {
+                throw 'no season on vd'
+            }
+
+            for (let rtpath of srts) {
+
+                let { base:rtbase, dir:rtdir, ext:rtext, name:rtname, root:rtroot } = path.parse(rtpath)
+    
+                let rtseason = this.libs.files.season(rtpath)
+                if (rtseason == null) {
+                    throw 'no season on rt'
+                }
+
+                if (kvseason != rtseason) {
+                    continue
+                }
+                
+                if ( kvname == rtname ) {
+
+                    // let outdir = path.join(kvdir, 'output')
+                    // if (!fs.existsSync(outdir)) {
+                    //     fs.mkdirSync(outdir, { recursive: false })
+                    // }
+
+                    // let outpath = path.join(outdir, kvbase)
+                    // let { msg } = await this.libs.ffmpeg.injectSubtitle(kvpath, rtpath, outpath)
+                    // if (msg != '') {
+                    //     throw msg
+                    // }
+
+                    break
+                }
+    
+            }
+        }
+
+        return this.msg(200, srts)
+    }
+}
