3cc2dc46054b7d4a328c8a5c85ba7f5cb5634c36
guandong <guandong@qq.com>
2022-04-20 15:44:04 +0800

    Update index.vue

--- lang/programming/nodejs/pmweb/src/components/index.vue
+++ lang/programming/nodejs/pmweb/src/components/index.vue
@@ -3,7 +3,8 @@
     <!-- <img src="../assets/play.gif"> -->
     keywd
     <!-- <input type="text" v-for="(item,i) of items" v-model="items[i]" :key="i"> <button @click="search">search</button> -->
-    <input v-model="keywdModel.keywd" placeholder="edit me" />  <button @click="search">search</button>
+    <input v-model="keywdModel.keywd" placeholder="edit me" />
+    <button @click="search">search</button>
     <p></p>
     <select v-model="keywdModel.lang_type">
       <option>jp</option>
@@ -14,31 +15,66 @@
       v-model="value1"
       multiple
       collapse-tags
-      style="margin-left: 20px;"
-      placeholder="animes">
+      style="margin-left: 20px"
+      placeholder="animes"
+    >
       <el-option
         v-for="item in options"
         :key="item.value"
         :label="item.label"
-        :value="item.value">
+        :value="item.value"
+      >
       </el-option>
     </el-select>
-    
+
     <!-- <p>keywd is: {{ keywdModel.keywd }}</p> -->
-  
+
     <p></p>
 
     <div class="result_main" v-if="isResultShow">
-
       <!-- <div v-html="resultModel.result"></div> -->
 
       <div v-for="item in resultsModel" :key="item.result">
-          <div v-html="item.result"></div>
-          <br>
+        <div v-html="item.result"></div>
+        <br />
       </div>
-
     </div>
 
+    <div class="result_image">
+      <el-collapse v-model="activeName" accordion>
+        <el-collapse-item title="一致性 Consistency" name="1">
+          <div>
+            与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；
+          </div>
+          <div>
+            在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。
+          </div>
+        </el-collapse-item>
+        <el-collapse-item title="反馈 Feedback" name="2">
+          <div>
+            控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；
+          </div>
+          <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+        </el-collapse-item>
+        <el-collapse-item title="效率 Efficiency" name="3">
+          <div>简化流程：设计简洁直观的操作流程；</div>
+          <div>
+            清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；
+          </div>
+          <div>
+            帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。
+          </div>
+        </el-collapse-item>
+        <el-collapse-item title="可控 Controllability" name="4">
+          <div>
+            用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；
+          </div>
+          <div>
+            结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。
+          </div>
+        </el-collapse-item>
+      </el-collapse>
+    </div>
 
     <!-- v-if 是条件渲染，每次状态更新都会重新删除或者创建元素，但v-if有较高的切换性能消耗 -->
     <!-- Vue 官方中不推荐v-for 和v-if 在同一标签中共同使用。因此，给上述示例代码外面加上一层div，isListShow 为true 时创建，为false 时销毁 -->
@@ -49,28 +85,25 @@
         </li>
       </ul>
     </div> -->
-
   </div>
 </template>
 
 <script>
+import config from "@/config.js";
+let host = config.server.host;
 
-import config from '@/config.js'
-let host = config.server.host
+import img_play from "../assets/play.gif";
+import img_play2 from "../assets/play2.gif";
 
-import img_play from '../assets/play.gif'
-import img_play2 from '../assets/play2.gif'
-
-
-const formurlencoded = require('form-urlencoded')
-const bent = require('bent')
+const formurlencoded = require("form-urlencoded");
+const bent = require("bent");
 
 export default {
-  name: 'index',
-  data () {
+  name: "index",
+  data() {
     return {
-      keywdModel:  { keywd: '', lang_type:'jp' },
-      resultModel: { result: '' },
+      keywdModel: { keywd: "", lang_type: "jp" },
+      resultModel: { result: "" },
       resultsModel: [],
       isResultShow: false,
       items: [],
@@ -85,55 +118,54 @@ export default {
         },
       ],
       value1: [],
-    }
+      activeName: "1",
+    };
   },
   computed: {
     itemNum: function () {
-      return this.items.length
-    }
+      return this.items.length;
+    },
   },
   methods: {
+    async search() {
+      let keywd = this.keywdModel.keywd;
+      let lang_type = this.keywdModel.lang_type;
 
-    async search () {
-
-      let keywd = this.keywdModel.keywd
-      let lang_type = this.keywdModel.lang_type
-
-      if (keywd == '') {
-        return
+      if (keywd == "") {
+        return;
       }
 
       //let host = 'localhost:81'
-      let url = `http://${host}`
+      let url = `http://${host}`;
       let json = {
         keywd,
-        type: 'anime',
-        lang_type
-      }
-      let formurlencoded_json = formurlencoded(json)
+        type: "anime",
+        lang_type,
+      };
+      let formurlencoded_json = formurlencoded(json);
 
-      let post = bent(url, 'POST', 'json', 200); //debugger
-      let response = await post('/search', formurlencoded_json, { 'Content-Type': 'application/x-www-form-urlencoded' })
+      let post = bent(url, "POST", "json", 200); //debugger
+      let response = await post("/search", formurlencoded_json, {
+        "Content-Type": "application/x-www-form-urlencoded",
+      });
 
       if (response.status == 200 && response.data.length > 0) {
-
         //let { id, jp, name, seasion, time, zh } = response.data[0]
         //let result = `${jp}<br>${zh}`
 
-        const data = []
+        const data = [];
         for (let { id, jp, type, name, seasion, time, zh } of response.data) {
           //let bs = process.env.BASE_URL; debugger
-          let elm_id = `${type}_${name}_${seasion}_${id}`
-          let au_url = `${url}/getaudio?type=${type}&name=${name}&seasion=${seasion}&id=${id}`
-          
-          //  
+          let elm_id = `${type}_${name}_${seasion}_${id}`;
+          let au_url = `${url}/getaudio?type=${type}&name=${name}&seasion=${seasion}&id=${id}`;
+
+          //
           let result = `${jp}<img id="img_${elm_id}" src="${img_play}" onclick="play('${elm_id}')"><audio id="audio_${elm_id}" src="${au_url}" type="audio/mpeg" preload="auto"></audio><br>${zh}`; //debugger
           //let result = `${jp}<div @click="play"><img id="img_${elm_id}" src="${img_play}"></div><audio id="audio_${elm_id}" src="${au_url}" type="audio/mpeg" preload="auto"></audio><br>${zh}`; //debugger
-          data.push( {result} )
+          data.push({ result });
         }
-        data.push({result:`<br><button onclick="next()">next</button>`})
+        data.push({ result: `<br><button onclick="next()">next</button>` });
 
-        
         // let audio_dir = path.join(global.animes.root_audio, type, name, seasion)
         // let audio_path = path.join(audio_dir, `${id}.mp3`)
         // http://127.0.0.1:80/getaudio?type=anime&name=danganronpa&seasion=S01&id=1
@@ -141,55 +173,53 @@ export default {
         // <audio id="@($"audio{row.id}")" src="@($"{url}")" type="audio/mpeg" preload="auto"></audio>
         // onclick="openImg()"
 
-        this.resultsModel = data
+        this.resultsModel = data;
 
         //this.$set(this.resultModel, 'result', result)  // 强制重绘
         // this.$set(this.keywdModel, 'keywd', 'aaaa')
 
         //this.resultModel.result = result
-        this.isResultShow = true
+        this.isResultShow = true;
         this.$nextTick(() => {
           // DOM 渲染完后回调
           //debugger
-        })
-
+        });
       } else {
-        console.log(`Waring: POST fail. ${url}/search`)
+        console.log(`Waring: POST fail. ${url}/search`);
       }
 
-
-      console.log('hited.')
-    }
+      console.log("hited.");
+    },
   },
-  mounted(){
-    window.play = function(elm_id) {
-      let auid = `audio_${elm_id}`
-      var igid = `img_${elm_id}`
+  mounted() {
+    window.play = function (elm_id) {
+      let auid = `audio_${elm_id}`;
+      var igid = `img_${elm_id}`;
 
-      let au = document.getElementById(auid)
-      let ig = document.getElementById(igid)
+      let au = document.getElementById(auid);
+      let ig = document.getElementById(igid);
       if (au.paused) {
-        au.play()
-        ig.src = img_play2
+        au.play();
+        ig.src = img_play2;
         // this.$nextTick(() => {
         //   // DOM 渲染完后回调
         //   //debugger
         // })
-        
+
         au.addEventListener("pause", function () {
-            ig.src = img_play
-        })
+          ig.src = img_play;
+        });
       }
       // var au = <HTMLAudioElement>document.getElementById(auid);
       // //var ig = <HTMLImageElement>document.getElementById("img"+id);
 
       // console.log(`openImg clicked. ${elm_id}`); debugger
-    }
-    let search = this.search
-    window.next = async function() {
-      await search()
-    }
-  }
+    };
+    let search = this.search;
+    window.next = async function () {
+      await search();
+    };
+  },
   // watch: {
 
   //   items (newVal) {
@@ -200,7 +230,7 @@ export default {
   //   }
 
   // }
-}
+};
 </script>
 
 <!--
