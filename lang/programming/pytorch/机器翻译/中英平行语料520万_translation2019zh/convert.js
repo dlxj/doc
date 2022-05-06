226c6e2a36de86860ea97294021030889f6e1a9d
guandong <guandong@qq.com>
2022-05-06 11:32:42 +0800

    Update convert.js

--- lang/programming/pytorch/机器翻译/中英平行语料520万_translation2019zh/convert.js
+++ lang/programming/pytorch/机器翻译/中英平行语料520万_translation2019zh/convert.js
@@ -16,6 +16,10 @@
         var data = await new Promise(function (resolve, reject) {
 
             var fs = require('fs')
+
+            fs.rmSync('en_chs', { recursive: true, force: true })
+            fs.mkdirSync('en_chs', { recursive: true })
+
             var src_train = fs.createWriteStream('en_chs/src-train.txt', {flags: 'a' }) // 'a' means appending (old data will be preserved)
             var tgt_train = fs.createWriteStream('en_chs/tgt-train.txt', {flags: 'a' }) // 'a' means appending (old data will be preserved)
 
@@ -23,36 +27,55 @@
             // src_train.write('more data') // again
             // src_train.write('and more') // again
 
+            let src_trains = []
+            let tgt_trains = []
+            let count = 0
 
             let lineReader = require('line-reader')
             lineReader.open(fpath, function(err, reader) {
                 if (err) return reject(err)
-                while (reader.hasNextLine()) {
+                while (reader.hasNextLine() && count <= 1000) {
+                  count += 1
                   reader.nextLine(function(err, line) {
                     try {
                         if (err) return reject(err)
                         if (line.trim() != '') {
                             line = JSON.parse(line)
-                            let english = clean(line.english)
+                            let english = line.english //clean(line.english)
                             english = english.replace(/(\,)/g, ' $1 ')  // OpenNMT 要通过空格分词，这里把标点和单词拆开
                             english = english.replace(/(\.)/g, ' $1 ')
                             english = english.replace(/(\?)/g, ' $1 ')
                             english = english.replace(/(\!)/g, ' $1 ')
                             english = english.replace(/([^\r\n\S]{2,999})/g, ' ')
 
-                            let chinese = clean(line.chinese)
+                            let chinese = line.chinese // clean(line.chinese)
                             chinese = chinese.replace(/(\，)/g, ' $1 ')  // OpenNMT 要通过空格分词，这里把标点和单词拆开
+                            chinese = chinese.replace(/(\、)/g, ' $1 ')
                             chinese = chinese.replace(/(\。)/g, ' $1 ')
                             chinese = chinese.replace(/(\？)/g, ' $1 ')
                             chinese = chinese.replace(/(\！)/g, ' $1 ')
                             chinese = chinese.replace(/(\《)/g, ' $1 ')
                             chinese = chinese.replace(/(\》)/g, ' $1 ')
-
+                            chinese = chinese.replace(/(\“)/g, ' $1 ')
+                            chinese = chinese.replace(/(\”)/g, ' $1 ')
+                            
                             chinese = chinese.replace(/([^\r\n\S]{2,999})/g, ' ')
                             // chinese = chinese.replace(/(\p{P})/gu, ' $1 ')  // 正则匹配所有中文标点
 
                             src_train.write(english + '\n') // append string to your file
                             tgt_train.write(chinese + '\n') // append string to your file
+
+                            src_trains.push( english + '\n' )
+                            tgt_trains.push( chinese + '\n' )
+
+
+                            if (count == 255) {
+                                let a = 1
+                            }
+
+                            //if (count % 10 == 0) {
+                                console.log(`${count} / unknow`)
+                            //}
 
                         }
                     } catch(err) {
@@ -65,9 +88,10 @@
 
                 reader.close(function(err) {
                     if (err) return reject(err)
-                    src_train.close()
-                    tgt_train.close()
-                    return resolve(result)
+                    src_train.end()
+                    tgt_train.end()
+                    console.log(`all task done.`)
+                    return resolve({msg:'ok.'})
                 })
             })
         })
@@ -80,8 +104,8 @@
         // let trainingstr = clean( fs.readFileSync(fpath, { encoding: 'utf8', flag: 'r' }) )
         // let trainingarr = trainingstr.split('\n')
     
-        let ens = ''
-        let chts = ''
+        let ens = 'file too large to return string'
+        let chts = '文件太大，无法返回字符串'
         // for (let s of trainingarr) {
         //     if (s == '') {
         //         continue
@@ -98,7 +122,7 @@
 
     }
 
-    let { ens:ens1, chts:chts1 } = await convert('./translation2019zh_train.json')
+    let { ens, chts } = await convert('./translation2019zh_train.json')
 
     // let { ens:ens2, chts:chts2 } = convert('./validation.txt')
 
@@ -113,6 +137,8 @@
 
     // require('fs').writeFileSync('src-test.txt', ens3, {encoding:'utf-8'} )
     // require('fs').writeFileSync('tgt-test.txt', chts3, {encoding:'utf-8'} )
+
+    let a = 1
 
 })()
 
