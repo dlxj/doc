4e177bf3a16cecd9a711f2adb3878409ac7cadfb
dlxj <123468935@qq.com>
2022-05-13 07:01:55 +0800

    m

--- lang/programming/nodejs/pmserver/http/api/genmkv/genmkvv2.js
+++ lang/programming/nodejs/pmserver/http/api/genmkv/genmkvv2.js
@@ -42,11 +42,6 @@ module.exports = {
                     throw 'no season on rt'
                 }
 
-                // let rtseason = this.libs.files.season(rtpath)
-                // if (rtseason == null) {
-                //     throw 'no season on rt'
-                // }
-
                 if (kvseason != rtseason) {
                     continue
                 }
@@ -73,27 +68,6 @@ module.exports = {
                                 fs.mkdirSync(outdir, { recursive: false })
                             }
 
-                            // let m3outpath = path.join(outdir, 'tmp.mp3')
-
-                            // let outpath = path.join(outdir, `${rtname}.mkv`)
-                            // let outpath2 = path.join(outdir, `${rtname}_add_audio.mkv`)
-
-                            // let begintime = `00:00:02.000`
-                            // //let endtime = `00:00:04.879`
-                            // let endtime = ``
-                            // let { au: audio } = await this.libs.ffmpeg.extractAudio(kvpath, 'mp3', begintime, endtime)  // 00:00:03.879  // https://superuser.com/questions/377343/cut-part-from-video-file-from-start-position-to-end-position-with-ffmpeg
-                            // if (audio == null) {
-                            //     throw `au is null. ${vdpath} ${begintime}`
-                            // }
-
-                            // require('fs').writeFileSync(m3outpath, audio )
-
-                            //let { msg } = await this.libs.ffmpeg.injectSubtitleAudio(m4path, rtpath, m3outpath, outpath)
-                            //let { msg:m1 } = await this.libs.ffmpeg.injectSubtitle(m4path, rtpath, outpath)
-                            //let { msg:m2 } = await this.libs.ffmpeg.injectSubtitle(outpath, m3outpath, outpath2)
-
-                            // fs.unlinkSync(m3outpath)
-
                             let outpath = path.join(outdir, `${rtname}.mkv`)
                             let { msg } = await this.libs.ffmpeg.merge_pokemonAmazon_pokemonTW(m4path, kvpath, rtpath, outpath)
 
@@ -104,16 +78,6 @@ module.exports = {
 
                     }
 
-                    // let outdir = path.join(kvdir, 'output')
-                    // if (!fs.existsSync(outdir)) {
-                    //     fs.mkdirSync(outdir, { recursive: false })
-                    // }
-
-                    // let outpath = path.join(outdir, kvbase)
-                    // let { msg } = await this.libs.ffmpeg.injectSubtitle(kvpath, rtpath, outpath)
-                    // if (msg != '') {
-                    //     throw msg
-                    // }
 
                     break
                 }
@@ -121,6 +85,84 @@ module.exports = {
             }
         }
 
+        await genkv_season3(type)
+
+
         return this.msg(200, 'ok.')
     }
+}
+
+async function genkv_season3(type) {
+    // S01 包含了S02
+    let platform = process.platform
+    let root_vd = global.config.root_vd[platform]
+
+    let kvs = this.libs.files.allfiles(root_vd, 'mkv', ['pokemon_amazon', 'S03'])  // E:\videos\anime\pokemon\amazon\S01
+
+    let m4s = this.libs.files.allfiles(root_vd, 'mp4', ['pokemon_tw', 'S03']) 
+
+    let srts = this.libs.files.allfiles(global.root_subtitles, 'srt', ['amazon', 'pokemon', 'srt', 'S03', 'jp'])
+
+    for (let kvpath of kvs) {
+
+        let { base:kvbase, dir:kvdir, ext:kvext, name:kvname, root:kvroot } = path.parse(kvpath)
+
+        let kvseason = this.libs.files.season(kvpath)
+        if (kvseason == null) {
+            throw 'no season on vd'
+        }
+
+        for (let rtpath of srts) {
+
+            let { base:rtbase, dir:rtdir, ext:rtext, name:rtname, root:rtroot } = path.parse(rtpath)
+
+            let rtseason = this.libs.files.season(rtpath)
+            if (rtseason == null) {
+                throw 'no season on rt'
+            }
+
+            if (kvseason != rtseason) {
+                continue
+            }
+            
+            if ( kvname == rtname ) {
+
+                for (let m4path of m4s) {
+
+                    let { base:m4base, dir:m4dir, ext:m4ext, name:m4name, root:m4root } = path.parse(m4path)
+
+                    let m4season = this.libs.files.season(m4path)
+                    if (m4season == null) {
+                        throw 'no season on m4'
+                    }
+
+                    if (m4season != rtseason) {
+                        continue
+                    }
+
+                    if ( m4name == rtname ) {
+
+                        let outdir = path.join(m4dir, 'output')
+                        if (!fs.existsSync(outdir)) {
+                            fs.mkdirSync(outdir, { recursive: false })
+                        }
+
+                        let outpath = path.join(outdir, `${rtname}.mkv`)
+                        let { msg } = await this.libs.ffmpeg.merge_pokemonAmazon_pokemonTW(m4path, kvpath, rtpath, outpath)
+
+
+                        break
+
+                    }
+
+                }
+
+
+                break
+            }
+
+        }
+    }
+
+
 }
--- lang/programming/nodejs/pmserver/lib/renamevd.js
+++ lang/programming/nodejs/pmserver/lib/renamevd.js
@@ -1,6 +1,7 @@
 
 let fs = require('fs')
 let path = require('path')
+let _ = require('lodash')
 
 module.exports = {
 
@@ -119,12 +120,199 @@ module.exports = {
 
         }
 
+        rename_season3()
+
         return 'ok.'
     }
 
 }
 
 
+function rename_season3() {
+    // S01包含了S02
+    let platform = process.platform
+    let root_vd = global.config.root_vd[platform]
+
+    let kvs = this.libs.files.allfiles(root_vd, 'mkv', ['pokemon', 'amazon', 'S03'])  // E:\videos\anime\pokemon\amazon\S01
+
+    let m4s = this.libs.files.allfiles(root_vd, 'mp4', ['pokemon_tw', 'S03']) 
+
+    let lvs = this.libs.files.allfiles(root_vd, 'flv', ['pokemon_tw', 'S03']) 
+
+    let ttml2s = this.libs.files.allfiles(global.root_subtitles, 'ttml2', ['amazon', 'pokemon', 'S03'])
+    // remove space
+    for (let ttml of ttml2s) {
+        let { base,dir,ext,name,root} = path.parse(ttml)
+        let withoutSpace = base.replace(/\s/g, '')
+        if (withoutSpace != base) {
+            let newname = path.join(dir, withoutSpace)
+            fs.renameSync( ttml, newname )
+        }
+    }
+    ttml2s = this.libs.files.allfiles(global.root_subtitles, 'ttml2', ['amazon', 'pokemon', 'S03'])
+
+    function sort(ttml) {
+        let match= ttml.match(/(\d+)\./)
+        if (match == null) {
+            throw `name not correct. ${ttml}`
+        }
+        let nth = match[1]
+        return Number(nth)
+    }
+
+    ttml2s = _.orderBy(ttml2s, [
+        function (item) { return sort(item); }
+      ], ["asc"])
+
+    for (let ttml of ttml2s) {
+
+        let match= ttml.match(/(\d+)\./)
+        if (match == null) {
+            //throw `name not correct. ${ttml}`
+            continue
+        }
+        let nth = match[1]
+
+        let mlseason = this.libs.files.season(ttml)
+        if (mlseason == null) {
+            throw 'no season on ttml2'
+        }
+
+        for (let kv of kvs) {
+
+            let match2 = kv.match(/E(\d+) - Amazon/)
+            if (match2 == null) {
+                //throw `name not correct. ${m4}`
+                continue
+            }
+            let nth2 = match2[1]
+
+            let kvseason = this.libs.files.season(kv)
+            if (kvseason == null) {
+                throw 'no season on kv'
+            }
+
+            if (mlseason != kvseason) {
+                continue
+            }
+
+            if (Number(nth) == Number(nth2)) {
+
+                //ttml
+                let { base,dir,ext,name,root} = path.parse(ttml)
+                let { base:base2,dir:dir2,ext:ext2,name:name2,root:root2} = path.parse(kv)
+
+                let newname = `${name}.mkv`
+                let newpath = path.join(dir2, newname)
+
+                fs.renameSync( kv, newpath )
+
+                continue
+            }
+
+        }
+
+        // 190 之前是mp4
+        for (let m4 of m4s) {
+
+            let match3 = m4.match(/(\d+)\_/)
+            if (match3 == null) {
+                //throw `name not correct. ${m4}`
+                continue
+            }
+            let nth3 = match3[1]
+
+            let m4season = this.libs.files.season(m4)
+            if (m4season == null) {
+                throw 'no season on kv'
+            }
+
+            if (mlseason != m4season) {
+                continue
+            }
+
+            // if (m4season == 'S01' && Number(nth3) >= 38) {
+            //     nth3 = Number(nth3) + 1
+            // }
+
+            if (Number(nth) <= 62 && Number(nth) == Number(nth3) - 117) {  
+
+                //ttml
+                let { base,dir,ext,name,root} = path.parse(ttml)
+                let { base:base3,dir:dir3,ext:ext3,name:name3,root:root3} = path.parse(m4)
+
+                let newname = `${name}.mp4`
+                let newpath = path.join(dir3, newname)
+
+                fs.renameSync( m4, newpath )
+            } else if ( Number(nth) >= 66 &&  Number(nth) == Number(nth3) - 114) {  // 63.ミュウツー！我ハココニ在リ(1) 到 65  台版无
+
+                //ttml
+                let { base,dir,ext,name,root} = path.parse(ttml)
+                let { base:base3,dir:dir3,ext:ext3,name:name3,root:root3} = path.parse(m4)
+
+                let newname = `${name}.mp4`
+                let newpath = path.join(dir3, newname)
+
+                fs.renameSync( m4, newpath )
+            }
+
+        }
+
+        // 191 之后是flv
+        for (let lv of lvs) {
+
+            let match3 = lv.match(/(\d+)\_/)
+            if (match3 == null) {
+                //throw `name not correct. ${m4}`
+                continue
+            }
+            let nth3 = match3[1]
+
+            let lvseason = this.libs.files.season(lv)
+            if (lvseason == null) {
+                throw 'no season on kv'
+            }
+
+            if (mlseason != lvseason) {
+                continue
+            }
+
+            // if (m4season == 'S01' && Number(nth3) >= 38) {
+            //     nth3 = Number(nth3) + 1
+            // }
+
+            // if (Number(nth) <= 63 && Number(nth) <= 65) {
+            //     let a = 1
+            // }
+
+            if ( Number(nth) <= 62 && Number(nth) == Number(nth3) - 117) {  // 63.ミュウツー！我ハココニ在リ(1) 到 65  台版无
+
+                //ttml
+                let { base,dir,ext,name,root} = path.parse(ttml)
+                let { base:base3,dir:dir3,ext:ext3,name:name3,root:root3} = path.parse(lv)
+
+                let newname = `${name}.flv`
+                let newpath = path.join(dir3, newname)
+
+                fs.renameSync( lv, newpath )
+            } else if ( Number(nth) >= 66 &&  Number(nth) == Number(nth3) - 114) {  // 63.ミュウツー！我ハココニ在リ(1) 到 65  台版无
+                //ttml
+                let { base,dir,ext,name,root} = path.parse(ttml)
+                let { base:base3,dir:dir3,ext:ext3,name:name3,root:root3} = path.parse(lv)
+
+                let newname = `${name}.flv`
+                let newpath = path.join(dir3, newname)
+
+                fs.renameSync( lv, newpath )
+            }
+
+        }
+
+    }
+
+    return 'ok.'
+}
 
 
 
