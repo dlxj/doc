67037600ee6ee0f8d2506c1f4d72d5c984421f2d
dlxj <123468935@qq.com>
2022-08-05 22:13:44 +0800

    s10

--- lang/programming/nodejs/pmserver/lib/renamevd.js
+++ lang/programming/nodejs/pmserver/lib/renamevd.js
@@ -124,6 +124,8 @@ module.exports = {
 
         rename_season6()
 
+        rename_season10()
+
         return 'ok.'
     }
 
@@ -457,7 +459,144 @@ function rename_season6() {
 }
 
 
+function rename_season10() {
 
+    let platform = process.platform
+    let root_vd = global.config.root_vd[platform]
+
+    let kvs = this.libs.files.allfiles(root_vd, 'mkv', ['pokemon', 'amazon', 'S10'])  // E:\videos\anime\pokemon\amazon\S01
+
+    // let lvs = this.libs.files.allfiles(root_vd, 'flv', ['pokemon_tw', 'S10'])
+
+    let ttml2s = this.libs.files.allfiles(global.root_subtitles, 'ttml2', ['amazon', 'pokemon', 'S10'])
+    // remove space
+    for (let ttml of ttml2s) {
+        let { base, dir, ext, name, root } = path.parse(ttml)
+        let withoutSpace = base.replace(/\s/g, '')
+        if (withoutSpace != base) {
+            let newname = path.join(dir, withoutSpace)
+            fs.renameSync(ttml, newname)
+        }
+    }
+    ttml2s = this.libs.files.allfiles(global.root_subtitles, 'ttml2', ['amazon', 'pokemon', 'S10'])
+
+    function sort(ttml) {
+        let match = ttml.match(/(\d+)\./)
+        if (match == null) {
+            throw `name not correct. ${ttml}`
+        }
+        let nth = match[1]
+        return Number(nth)
+    }
+
+    ttml2s = _.orderBy(ttml2s, [
+        function (item) { return sort(item); }
+    ], ["asc"])
+
+    for (let ttml of ttml2s) {
+
+        let match = ttml.match(/(\d+)\./)
+        if (match == null) {
+            //throw `name not correct. ${ttml}`
+            continue
+        }
+        let nth = match[1]
+
+        let mlseason = this.libs.files.season(ttml)
+        if (mlseason == null) {
+            throw 'no season on ttml2'
+        }
+
+        for (let kv of kvs) {
+
+            let match2 = kv.match(/E(\d+) - Amazon/)
+            if (match2 == null) {
+                //throw `name not correct. ${m4}`
+                continue
+            }
+            let nth2 = match2[1]
+
+            let kvseason = this.libs.files.season(kv)
+            if (kvseason == null) {
+                throw 'no season on kv'
+            }
+
+            if (mlseason != kvseason) {
+                continue
+            }
+
+            if (Number(nth) == Number(nth2)) {
+
+                //ttml
+                let { base, dir, ext, name, root } = path.parse(ttml)
+                let { base: base2, dir: dir2, ext: ext2, name: name2, root: root2 } = path.parse(kv)
+
+                let newname = `${name}.mkv`
+                let newpath = path.join(dir2, newname)
+
+                fs.renameSync(kv, newpath)
+
+                continue
+            }
+
+        }
+
+        // for (let lv of lvs) {
+
+        //     let match3 = lv.match(/(\d+)\./)
+        //     if (match3 == null) {
+        //         //throw `name not correct. ${m4}`
+        //         continue
+        //     }
+        //     let nth3 = match3[1]
+
+        //     let lvseason = this.libs.files.season(lv)
+        //     if (lvseason == null) {
+        //         throw 'no season on kv'
+        //     }
+
+        //     if (mlseason != lvseason) {
+        //         continue
+        //     }
+
+        //     // if (Number(nth) <= 119 && Number(nth) == Number(nth3)) {  // 120 原版是总集编，台版没有。 台120 对应原121
+
+        //     //     //ttml
+        //     //     let { base, dir, ext, name, root } = path.parse(ttml)
+        //     //     let { base: base3, dir: dir3, ext: ext3, name: name3, root: root3 } = path.parse(lv)
+
+        //     //     let newname = `${name}.flv`
+        //     //     let newpath = path.join(dir3, newname)
+
+        //     //     fs.renameSync(lv, newpath)
+
+        //     //     break
+
+        //     // }
+
+            
+        //     // if (Number(nth) >= 121 && Number(nth) == Number(nth3) +1 ) {  // 120 原版是总集编，台版没有。 台120 对应原121
+
+        //     //     //ttml
+        //     //     let { base, dir, ext, name, root } = path.parse(ttml)
+        //     //     let { base: base3, dir: dir3, ext: ext3, name: name3, root: root3 } = path.parse(lv)
+
+        //     //     let newname = `${name}.flv`
+        //     //     let newpath = path.join(dir3, newname)
+
+        //     //     fs.renameSync(lv, newpath)
+
+        //     //     break
+
+        //     // }
+
+
+        // }
+
+    }
+
+    return 'ok.'
+}
 
 
 
